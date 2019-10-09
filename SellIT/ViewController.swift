75312bc4c36fcc9fe85ff0fd7f35a63877b09f8b
Himanshu Chaurasiya <Himanshu.Chaurasiya@donseed.com>
2019-10-09 11:34:48 +0530

    commit

---
+++ SellIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SellIT
+//
+//  Created by Himanshu Chaurasiya on 09/10/19.
+//  Copyright © 2019 MyTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
